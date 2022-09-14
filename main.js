7bd22c3fb17ca40cd7a5b3c244ab03d8faaad883
Ayan Banerjee <ayanbanerjee102@gmail.com>
2022-09-14 22:39:49 +0530

    Added the Form add comment functionality

--- main.js
+++ main.js
@@ -1,1 +1,30 @@
+import { v4 as uuidv4 } from "https://jspm.dev/uuid";
 
+const form = document.querySelector(".form");
+const formInputs = document.querySelectorAll("form input");
+
+let formValues = {};
+
+let state = {
+  todos: [],
+};
+
+// Functions
+function submitForm(e) {
+  e.preventDefault();
+  state.todos.push(formValues);
+  clearUI();
+  console.log(state.todos);
+}
+
+function getFormValues(e) {
+  formValues = { ...formValues, [e.target.name]: e.target.value, id: uuidv4() };
+}
+
+function clearUI() {
+  formInputs.forEach((item) => (item.value = ""));
+}
+
+// Add EvenetListiners
+form.addEventListener("submit", submitForm);
+form.addEventListener("change", getFormValues);
